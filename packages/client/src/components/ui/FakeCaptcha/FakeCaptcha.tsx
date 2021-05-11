4bcd186c2fb8ab7398e92117aba7486aebbb1fbf
Konstantin Darutkin <spalt08@mail.ru>
2021-05-11 14:23:30 +0300

    feat(client): good-looking captcha for Tor

--- packages/client/src/components/ui/FakeCaptcha/FakeCaptcha.tsx
+++ packages/client/src/components/ui/FakeCaptcha/FakeCaptcha.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import captchaImage from 'assets/captcha.png'
+import volumeIcon from 'assets/volume.svg'
 import styles from './FakeCaptcha.module.css'
 
 export function FakeCaptcha() {
+  useEffect(() => {
+    document.body.style.overflow = 'hidden'
+  
+    return () => {
+      document.body.style.overflow = 'visible'
+    }
+  }, [])
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <h3>Enter Captcha</h3>
-        <p>Any 24 random chars (will be picture there)</p>
-        <input type="text" />
-      </div>
+    <div className={styles.container}>
+      <h3>Please verify that you are a human</h3>
+      <img src={captchaImage} className={styles.image} alt="captcha" /><img src={volumeIcon} className={styles.volume} />
+      <br/>
+
+      <input type="text" className={styles.input} placeholder='Enter the words, separated by spaces' />
+      <br/>
+
+      {/* <div className={styles.button}>Verify</div> */}
     </div>
   )
 }
