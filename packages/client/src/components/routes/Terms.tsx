4ffb3ac9ca88b00544bb83a81ad687d51fe0b566
Konstantin Darutkin <spalt08@mail.ru>
2021-05-12 16:58:45 +0300

    feat(client): terms of use

--- packages/client/src/components/routes/Terms.tsx
+++ packages/client/src/components/routes/Terms.tsx
@@ -4,8 +4,68 @@ import { Centered } from 'components/ui'
 export function Terms() {
   return (
     <Centered>
-      <h2>Terms Page</h2>
-      <p>TBD</p>
+      <div style={{ textAlign: 'left' }}>
+        <h2>Terms of Use</h2>
+        <p>
+          These Terms of Use constitute a legally binding agreement made between you, whether personally or on behalf of an entity (“you”) and FingerprintJS, Inc ("Company", “we”, “us”, or “our”), concerning your access to and use of the https://schemeflood.com website (further referred to collectively as the “Site”).
+        </p>
+        <p>
+          You agree that by accessing the Site, you have read, understood, and agreed to be bound by all of these Terms of Use. IF YOU DO NOT AGREE WITH ALL OF THESE TERMS OF USE, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SITE AND YOU MUST DISCONTINUE USE IMMEDIATELY.
+        </p>
+        <p>
+        The Site is intended for users who are at least 13 years of age. 
+        </p>
+
+        <h3>USER REPRESENTATIONS</h3>
+        <p>
+          By using the Site, you represent and warrant that:  (1) you have the legal capacity and you agree to comply with these Terms of Use; (2) you are not under the age of 13; (3) you are not a minor in the jurisdiction in which you reside, or if a minor, you have received parental permission to use the Site; (4) you will not access the Site through automated or non-human means, whether through a bot, script, or otherwise; (5) you will not use the Site for any illegal or unauthorized purpose; and (6) your use of the Site will not violate any applicable law or regulation.
+        </p>
+        <p>
+          If you provide any information that is untrue, inaccurate, not current, or incomplete, we have the right to suspend or terminate your account and refuse any and all current or future use of the Site (or any portion thereof).
+        </p>
+
+        <h3>GOVERNING LAW</h3>
+        <p>
+          These Terms of Use and your use of the Site are governed by and construed in accordance with the laws of the State of Delaware applicable to agreements made and to be entirely performed within the State of Delaware, without regard to its conflict of law principles.
+        </p>
+
+        <h3>DISCLAIMER</h3>
+        <p>
+          THE SITE IS PROVIDED ON AN AS-IS AND AS-AVAILABLE BASIS. YOU AGREE THAT YOUR USE OF THE SITE AND OUR SERVICES WILL BE AT YOUR SOLE RISK. TO THE FULLEST EXTENT PERMITTED BY LAW, WE DISCLAIM ALL WARRANTIES, EXPRESS OR IMPLIED, IN CONNECTION WITH THE SITE AND YOUR USE THEREOF, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND NON-INFRINGEMENT. WE MAKE NO WARRANTIES OR REPRESENTATIONS ABOUT THE ACCURACY OR COMPLETENESS OF THE SITE’S CONTENT OR THE CONTENT OF ANY WEBSITES LINKED TO THE SITE AND WE WILL ASSUME NO LIABILITY OR RESPONSIBILITY FOR ANY (1) ERRORS, MISTAKES, OR INACCURACIES OF CONTENT AND MATERIALS, (2) PERSONAL INJURY OR PROPERTY DAMAGE, OF ANY NATURE WHATSOEVER, RESULTING FROM YOUR ACCESS TO AND USE OF THE SITE, (3) ANY UNAUTHORIZED ACCESS TO OR USE OF OUR SECURE SERVERS AND/OR ANY AND ALL PERSONAL INFORMATION AND/OR FINANCIAL INFORMATION STORED THEREIN, (4) ANY INTERRUPTION OR CESSATION OF TRANSMISSION TO OR FROM THE SITE, (5) ANY BUGS, VIRUSES, TROJAN HORSES, OR THE LIKE WHICH MAY BE TRANSMITTED TO OR THROUGH THE SITE BY ANY THIRD PARTY, AND/OR (6) ANY ERRORS OR OMISSIONS IN ANY CONTENT AND MATERIALS OR FOR ANY LOSS OR DAMAGE OF ANY KIND INCURRED AS A RESULT OF THE USE OF ANY CONTENT POSTED, TRANSMITTED, OR OTHERWISE MADE AVAILABLE VIA THE SITE. WE DO NOT WARRANT, ENDORSE, GUARANTEE, OR ASSUME RESPONSIBILITY FOR ANY PRODUCT OR SERVICE ADVERTISED OR OFFERED BY A THIRD PARTY THROUGH THE SITE, ANY HYPERLINKED WEBSITE, OR ANY WEBSITE OR MOBILE APPLICATION FEATURED IN ANY BANNER OR OTHER ADVERTISING, AND WE WILL NOT BE A PARTY TO OR IN ANY WAY BE RESPONSIBLE FOR MONITORING ANY TRANSACTION BETWEEN YOU AND ANY THIRD-PARTY PROVIDERS OF PRODUCTS OR SERVICES. AS WITH THE PURCHASE OF A PRODUCT OR SERVICE THROUGH ANY MEDIUM OR IN ANY ENVIRONMENT, YOU SHOULD USE YOUR BEST JUDGMENT AND EXERCISE CAUTION WHERE APPROPRIATE.
+        </p>
+
+        <h3>LIMITATIONS OF LIABILITY</h3>
+        <p>
+          IN NO EVENT WILL WE OR OUR DIRECTORS, EMPLOYEES, OR AGENTS BE LIABLE TO YOU OR ANY THIRD PARTY FOR ANY DIRECT, INDIRECT, CONSEQUENTIAL, EXEMPLARY, INCIDENTAL, SPECIAL, OR PUNITIVE DAMAGES, INCLUDING LOST PROFIT, LOST REVENUE, LOSS OF DATA, OR OTHER DAMAGES ARISING FROM YOUR USE OF THE SITE, EVEN IF WE HAVE BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES.
+        </p>
+
+        <h3>USER DATA</h3>
+        <p>
+          We will collect and maintain certain data that you transmit to the Site for the purpose of fulfilling the core service of the Site, as well as data relating to your use of the Site. 
+        </p>
+        <p>
+          Data collected on Site is used solely and exclusively for research purposes. We will not share, sell, or make available the data that we collect on Site in part or in full to any company or individual who is not an employee, contractor or an affiliate of FingerprintJS, Inc.
+        </p>
+        <p>
+          Data collected includes, but is not limited to browser fingerprinting information and any other information publicly exposed by web browsers.
+        </p>
+        <p>
+          We will retain the data collected for the period necessary to perform the core service of the Site.
+        </p>
+        <p>
+          This Site may also use cookies that are necessary to perform the core service of the Site.
+          Cookies are stored for a period of 1 year.
+        </p>
+        
+        <h3>CONTACT US </h3>
+        <p>
+          In order to resolve a complaint regarding the Site or to receive further information regarding use of the Site, please contact us at: 
+        </p>
+        <p>
+          FingerprintJS, Inc<br/>
+          <a href="mailto:oss@fingerprintjs.com">oss@fingerprintjs.com</a>
+        </p>
+      </div>
     </Centered>
   )
 }
